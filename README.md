452801eec30cc5e6da79b1f4b51f370c6406183e
MiMNets <mdmonirulislammonir@gmail.com>
2020-10-04 00:31:10 +0600

    Server ready

---
+++ README.md
@@ -0,0 +1,9 @@
+# volunteer-network-server
+echo "# volunteer-network-server" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/mimnets/volunteer-network-server.git
+git push -u origin main
+                
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const ObjectID = require('mongodb').ObjectID;
+
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ak6zw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const port = 3001
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+client.connect(err => {
+  const volunteerCollections = client.db('volunteerNetwork').collection('volunteerServices');
+  // perform actions on the collection object
+    app.get("/home", (req, res) => {
+        volunteerCollections.find({})
+        .toArray((err, documents) =>{
+            // console.log(documents);
+            // res.send('data base connected');
+            res.send(documents)
+        })
+    })
+});
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port)
